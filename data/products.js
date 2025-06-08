4f4a8d7a6057e0dbb52fa51241822bfd9855f46a
Judith Villalvazo <157530840+judith-vm@users.noreply.github.com>
2025-06-07 22:32:21 -0600

    Add files via upload

---
+++ data/products.js
@@ -0,0 +1,135 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Aretes Concha',
+    description: 'Aretes de acero inoxidable 3x3cm',
+    price: 109,
+    image: '/products/AretesConcha.png'
+  },
+  {
+    id: 2,
+    name: 'Aretes Hoja',
+    description: 'Aretes de acero inoxidable 3.5x2.5cm',
+    price: 109,
+    image: '/products/AretesHoja.png'
+  },
+   {
+    id: 3,
+    name: 'Aretes Perla',
+    description: 'Aretes de acero inoxidable 2x2cm',
+    price: 69,
+    image: '/products/AretesPerla.png'
+  },
+  {
+    id: 4,
+    name: 'Aretes Rayas',
+    description: 'Aretes acero inoxidable color plata 2.5x2.5cm',
+    price: 99,
+    image: '/products/AretesRayas.png'
+  },
+  {
+    id: 5,
+    name: 'Coquetas Chunky',
+    description: 'Coquetas de acero inoxidable 2x2cm',
+    price: 89,
+    image: '/products/CoquetasChunky.png'
+  },
+   {
+    id: 6,
+    name: 'Choker Corazones',
+    description: 'Choker acero inoxidable 41cm',
+    price: 109,
+    image: '/products/ChokerCorazones.png'
+  },
+   {
+    id: 7,
+    name: 'Choker Flores',
+    description: 'Choker acero inoxidable 41cm',
+    price: 109,
+    image: '/products/ChokerFlores.png'
+  },
+   {
+    id: 8,
+    name: 'Choker Ojos',
+    description: 'Choker acero inoxidable blanco con azul 41cm',
+    price: 129,
+    image: '/products/ChokerOjos1.png'
+  },
+   {
+    id: 9,
+    name: 'Choker Ojos',
+    description: 'Choker acero inoxidable morado con blanco 41cm',
+    price: 129,
+    image: '/products/ChokerOjos2.png'
+  },
+  {
+    id: 10,
+    name: 'Pulsera concha',
+    description: 'Pulsera tejida ajustable',
+    price: 59,
+    image: '/products/PulseraConcha.png'
+  },
+  {
+    id: 11,
+    name: 'Pulsera Corazon',
+    description: 'Pulsera acero inoxidable 18cm',
+    price: 59,
+    image: '/products/PulseraCorazon.png'
+  },
+  {
+    id: 12,
+    name: 'Pulsera Ojo',
+    description: 'Pulsera ajustable acero inoxidable',
+    price: 79,
+    image: '/products/PulseraOjo.png'
+  },
+  {
+    id: 13,
+    name: 'Pulsera Conchas',
+    description: 'Pulsera tejida ajustable',
+    price: 69,
+    image: '/products/PulseraConchas.png'
+  },
+  {
+    id: 14,
+    name: 'Anillo Ojo',
+    description: 'Anillo ajustable acero inoxidable',
+    price: 79,
+    image: '/products/AnilloOjo.png'
+  },
+  {
+    id: 15,
+    name: 'Anillo Sol',
+    description: 'Anillo ajustable acero inoxidable',
+    price: 79,
+    image: '/products/AnilloSol.png'
+  },
+   {
+    id: 16,
+    name: 'Collar Corazon',
+    description: 'Collar acero inoxidable 48cm',
+    price: 89,
+    image: '/products/CollarCorazon.png'
+  },
+   {
+    id: 17,
+    name: 'Collar Serpiente',
+    description: 'Collar corto acero inoxidable color plata 42cm',
+    price: 99,
+    image: '/products/CollarSerpiente.png'
+  },
+   {
+    id: 18,
+    name: 'Collar Estrellas',
+    description: 'Collar acero inoxidable 46cm',
+    price: 139,
+    image: '/products/CollarEstrellas.png'
+  },
+   {
+    id: 19,
+    name: 'Collar Corazones',
+    description: 'Collar corto acero inoxidable 42cm',
+    price: 89,
+    image: '/products/CollarCorazones.png'
+  }
+];
